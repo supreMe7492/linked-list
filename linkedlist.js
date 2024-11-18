48bc6facaef3dfbdd8b5043a0c3788e37995180a
Supreme Raut <supremeraut94@gmail.com>
2024-11-18 22:02:59 +0545

    linked list completed

--- linkedlist.js
+++ linkedlist.js
@@ -21,8 +21,10 @@ class linkedList {
         current = current.nextNode;
       }
       current.nextNode = node;
-      console.log(current);
+
+      //console.log(current);
     }
+
     this.length++;
   }
 
@@ -36,15 +38,96 @@ class linkedList {
       node.nextNode = current.nextNode;
       current.nextNode = node;
 
-      console.log(current);
+      //console.log(current);
     }
     this.length++;
   }
+  size() {
+    return this.length;
+  }
+  getHead() {
+    return this.head.value;
+  }
+  getTail() {
+    let current = this.head;
+    while (current.nextNode != null) {
+      current = current.nextNode;
+    }
+    return current.value;
+  }
+  at(index) {
+    let current = this.head;
+    let currentIndex = 0;
+    while (currentIndex < index) {
+      current = current.nextNode;
+      currentIndex++;
+    }
+    return current.value;
+  }
+  pop() {
+    if (this.head == null) {
+      return;
+    }
+    if (this.head.nextNode == null) {
+      this.head = null;
+      this.length--;
+      return;
+    }
+
+    let current = this.head;
+    while (current.nextNode.nextNode != null) {
+      current = current.nextNode;
+    }
+    current.nextNode = null;
+    this.length--;
+  }
+  contains(value) {
+    if (this.head.value == value) {
+      return true;
+    }
+    let current = this.head;
+    while (current.nextNode != null) {
+      current = current.nextNode;
+      if (current.value == value) {
+        return true;
+      }
+    }
+    return false;
+  }
+  find(value) {
+    let index = 0;
+    let current = this.head;
+    if (this.head.value == value) {
+      return index;
+    }
+    while (current.value != value) {
+      current = current.nextNode;
+      index++;
+      if (current == null) {
+        return `Nothing with (${value}) is in this node`;
+      }
+    }
+
+    return index;
+  }
+  toString() {
+    let current = this.head;
+    let values = [];
+    while (current != null) {
+      values.push(current.value);
+      current = current.nextNode;
+    }
+    return ` ${values.join(" -> ")} -> null`;
+  }
 }
 
-let a = new linkedList();
-a.append("ASAS");
-a.append("fuck");
-a.append("suii");
-//a.prepend("nahh");
-//console.log(a);
+const list = new linkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
